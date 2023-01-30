a6c8687f2e4c9267fabbc7bc161c7a615cb82393
enotstitch <kessler.anna96@gmail.com>
2023-01-30 14:36:25 +0300

    Adding word declension

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ const date = document.querySelector('.day-counter__date');
 const yearsBlock = document.querySelector('.result-item__num--years');
 const daysBlock = document.querySelector('.result-item__num--days');
 const hoursBlock = document.querySelector('.result-item__num--hours');
+const yearsText = document.querySelector('.result-item__text--years');
+const daysText = document.querySelector('.result-item__text--days');
+const hoursText = document.querySelector('.result-item__text--hours');
+
+console.log(daysText);
 
 flatpickrConfig = {
   locale: Russian,
@@ -20,6 +25,15 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   count();
 });
+
+function declOfNum(number, titles) {
+  cases = [2, 0, 1, 1, 1, 2];
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ];
+}
 
 const count = () => {
   const dateValue = moment(date.value);
@@ -44,4 +58,8 @@ const count = () => {
 
   let hours = Math.round(dateDiff / 1000 / 60 / 60) % 24;
   hoursBlock.textContent = hours;
+
+  yearsText.textContent = declOfNum(years, ['год', 'года', 'лет']);
+  daysText.textContent = declOfNum(days, ['день', 'дня', 'дней']);
+  hoursText.textContent = declOfNum(hours, ['час', 'часа', 'часов']);
 };
